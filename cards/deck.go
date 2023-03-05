261d36bdb384106ab190e816c6530289d6efb4be
Armağan Gök <1armagangok@gmail.com>
2023-03-05 16:17:45 +0300

    .

---
+++ cards/deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func (myDeck deck) print() {
+	for _, d := range myDeck {
+		fmt.Println(d)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
